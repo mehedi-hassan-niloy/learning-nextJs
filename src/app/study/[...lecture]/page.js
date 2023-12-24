ed1793cbe695163f721c2130b6128cb0c70f1b09
mehedi-hassan-niloy <mehedihasanniloy07@fgmail.com>
2023-12-24 22:56:06 +0600

    Catch All Segment impliment

---
+++ src/app/study/[...lecture]/page.js
@@ -0,0 +1,11 @@
+'use client'
+export default function Lecture({params}){
+    console.log(params)
+    return(
+        <div>
+            <h1>Day of Collage : {params.lecture[0]}</h1>
+            <h1>Lecture No :{params.lecture[1]}</h1>
+            
+        </div>
+    )
+}
