f561f7f1462fb3e3cccff80e6f6b7f3d05e99e25
mehedi-hassan-niloy <mehedihasanniloy07@fgmail.com>
2023-12-24 18:30:34 +0600

    conditional layout impliment

--- src/app/about/layout.js
+++ src/app/about/layout.js
@@ -1,13 +1,18 @@
+'use client'
 import Link from "next/link";
 import './about.css'
+import { usePathname } from "next/navigation";
 
 export default function Layout({children}){
+    const pathName = usePathname()
+    console.log(pathName)
     return(
         <div>
-            <ul className="about-menu">
+            {
+                pathName !=="/about/aboutteacher"?<ul className="about-menu">
                 <h4 className="Heading">About Navbar</h4>
                 <li>
-                    <Link href='/login'>About Main</Link>
+                    <Link href='/about'>About Main</Link>
                 </li>
                 <li>
                     <Link href='/about/aboutstudent'>About Student</Link>
@@ -16,6 +21,9 @@ export default function Layout({children}){
                     <Link href='/about/aboutteacher'>About Teacher</Link>
                 </li>
             </ul>
+                
+                :<Link href='/about'>back to main about page</Link>
+            }
             {
              children   
             }
--- src/app/login/layout.js
+++ src/app/login/layout.js
@@ -1,21 +1,29 @@
+'use client'
 import Link from "next/link";
 import './login.css'
+import { usePathname } from "next/navigation";
 
 export default function Layout({children}){
+    const pathName = usePathname()
+    console.log(pathName)
     return(
         <div>
-            <ul className="login-menu">
-                <h4 className="Heading">Login Navbar</h4>
-                <li>
-                    <Link href='/login'>Login Main</Link>
-                </li>
-                <li>
-                    <Link href='/login/loginstudent'>Login Student</Link>
-                </li>
-                <li>
-                    <Link href='/login/loginteacher'>Login Teacher</Link>
-                </li>
-            </ul>
+          {
+          pathName !== "/login/loginteacher"?
+          <ul className="login-menu">
+           <h4 className="Heading">Login Navbar</h4>
+           <li>
+               <Link href='/login'>Login Main</Link>
+           </li>
+           <li>
+               <Link href='/login/loginstudent'>Login Student</Link>
+           </li>
+           <li>
+               <Link href='/login/loginteacher'>Login Teacher</Link>
+           </li>
+       </ul> 
+          :<Link href='/login'>back to main login page</Link>
+          }
             {
              children   
             }
